f17c37841015d203ef07e897bc3d888f82d4c017
twngbm <twngbm@gmail.com>
2020-08-12 21:01:00 +0800

    File Sorted.

--- testControler.py
+++
@@ -1,7 +0,0 @@
-import solution
-import os
-import json
-__PATH__=os.path.dirname(os.path.abspath(__file__))
-with open(__PATH__+"/tree.json","r") as f:
-    solutionTree=json.load(f)
-Parser=solution.QAParser()
--- treeCreator.py
+++ treeCreator.py
@@ -1,81 +1,102 @@
 ########################
-#Node={"Key":{
-#    "featureText":["Text To Explain Feature",],
-#    "featureName":"Feature Name", #變數名稱
-#    "inputParser":["Input Transform Function Name",], #function name
-#    "featureKey":{"Key":*Node1,"Key2":*Node2},
-#    "defaultAnswer":{"str":"Answer Text.","function":"Function Name"},
-#    "ParentNode":*parentNode}
-#}
+# Node={Feature:
+#           {<FeatureName>:
+#               {"InputParser":[<Parser Name>],
+#                   Key:{"KeyName":<Next Node>}
+#                }},
+#       BalanceFlag:True/False #此層以下開始進行亂數詢問
+#       "Answer":<Answer Text>}
 ########################
 import json
 import os
 import logging
 import time
-global dormPath
-dormNetOveruse={
-    "featureText":["You use too much."],
-    "featureName":None,
-    "inputTrans":None,
-    "featureKey":None,
-    "defaultAnswer":None
+Feature = "Feature"
+InputParser = "InputParser"
+Key = "Key"
+Answer = "Answer"
+BalanceFlag = "BalanceFlag"
+DormOveruse = {Answer: "您已超流量(24小時內超過8GB)"}
+DormHWDead = {Answer: "您所在宿舍/樓層之網路設備目前異常中"}
+isDHCP = {Answer: "請聯絡61010由專人為您服務"}  #未註冊，註冊裝置MAC改變
+noDHCP = {Answer: "請設定為DHCP"}
+DormHWAlive = {
+    Feature: {
+        "isDHCP": {
+            InputParser: ["BooleanParser"],
+            Key: {
+                True: isDHCP,
+                "False": noDHCP
+            }
+        }
+    }
 }
-labPath={"featureText":"Ask yourself.",
-    "featureName":None,
-    "inputTrans":None,
-    "featureKey":None,
-    "defaultAnswer":None}
-userNetHWSuccess={"featureText":"Wait and Retry.",
-    "featureName":None,
-    "inputTrans":None,
-    "featureKey":None,
-    "defaultAnswer":None}
-userNetHWFailed={"featureText":"Plug the cable in.",
-    "featureName":None,
-    "inputTrans":None,
-    "featureKey":None,
-    "defaultAnswer":None}
-dormNetDebug={"featureText":"Please Check Your Socket and Connection",
-    "featureName":"UserHWConnectionStatus",
-    "inputTrans":None,
-    "featureKey":{"正常":userNetHWSuccess,"異常":userNetHWFailed},
-    "defaultAnswer":{"str":"StudentID Not Found"}}
-dormPath={
-    "featureText":"Please Enter Your Student ID.",
-    "featureName":"StudentID",
-    "inputTrans":["checkDormNetIDUsage"],
-    "featureKey":{True:dormNetOveruse,False:dormNetDebug},
-    "defaultAnswer":{"str":"StudentID Not Found"}
+DormNonOveruse = {
+    Feature: {
+        "DormID": {
+            InputParser: ["DormHWChecker"],
+            Key: {
+                "HWDead": DormHWDead,
+                "HWAlive": DormHWAlive
+            }
+        }
+    }
 }
-test1={"featureText":"You Should Have No Issue.",
-    "featureName":None,
-    "inputTrans":None,
-    "featureKey":None,
-    "defaultAnswer":None}
-test2={"featureText":"Use VPM.",
-    "featureName":None,
-    "inputTrans":None,
-    "featureKey":None,
-    "defaultAnswer":None}
-a={"featureText":"What Wrong With You?",
-   "featureName":"Keyword",
-   "inputTrans":["CKIPParser"],
-   "featureKey":{
-    "網路":{
-    "featureText":"Where is your location?",
-    "featureName":"Location",
-    "inputTrans":None,
-    "featureKey":{"宿舍":dormPath,"實驗室":labPath},
-    "defaultAnswer":{"str":"Call 61010"}},
-"授權軟體":{"featureText":"Where is your location?",
-    "featureName":"Location",
-    "inputTrans":None,
-    "featureKey":{"校內":test1,"校外":test2},
-    "defaultAnswer":{"str":"請輸入校內或校外"}}},
-    "defaultAnswer":{"function":"Google"}
+Dorm = {
+    Feature: {
+        "StudentID": {
+            InputParser: ["DormIDUsage"],
+            Key: {
+                "Overuse": DormOveruse,
+                "Non-Overuse": DormNonOveruse
+            }
+        }
+    },
+    BalanceFlag: True
+}  #床位已滿，網路孔報修
+Lab = {Answer: "請聯絡貴系網管進行處理"}
+School = {Answer: "請聯絡貴單位網管進行處理"}
+LAN = {
+    Feature: {
+        "Location": {
+            InputParser: ["LanLocationParser"],
+            Key: {
+                "宿舍": Dorm,
+                "系館": Lab,
+                "行政單位": School
+            }
+        }
+    }
 }
+StudentWAN={}
+ProfWAN={}
+NckuWAN={Feature:{"UserIdentifity":{InputParser:[],Key:{"學生":StudentWAN,"教授":ProfWAN}}},BalanceFlag:True}
+NoneNckuWAN={}
+WAN = {Feature:{"UserDomain":{InputParser:[],Key:{"校內人士":NckuWAN,"校外人士":NoneNckuWAN}}}}
 
-__PATH__=os.path.dirname(os.path.abspath(__file__))
-with open(__PATH__+"/tree.json","w") as f:
-    json.dump({"root":a},f)
-
+Network = {
+    Feature: {
+        "ConnectionType": {
+            InputParser: [],
+            Key: {
+                "有線": LAN,
+                "無線": WAN
+            }
+        }
+    }
+}
+CAS={}
+root = {
+    Feature: {
+        "Keyword": {
+            InputParser: ["CKIPParser"],
+            Key: {
+                "網路": Network,
+                "授權軟體":CAS #Campus Authorized Software
+            }
+        }
+    }
+}
+__PATH__ = os.path.dirname(os.path.abspath(__file__))
+with open(__PATH__ + "/tree.json", "w") as f:
+    json.dump(root, f)
