ffd2608c33a820cc4915e20a9f74e620840f4d06
nckuoverload <q36071017@gs.ncku.edu.tw>
2020-09-28 17:35:49 +0800

    UI version2

--- html/version2/index.js
+++ html/version2/index.js
@@ -1,24 +1,17 @@
-var ws = new WebSocket("ws://140.116.72.233:8000");
-var token;
+var ws = new WebSocket("ws://140.116.72.242:8080");
 var time = Date($.now());
 var scro;
 
 $(window).on('load',function () {
-  scro = $("#chat").mCustomScrollbar();
+  init();
 });
-
 function init(){
   scro = $("#chat").mCustomScrollbar();
-  
-}
-
-function li_onclick(name){
-  console.log(name);
 }
 
 //websocket
 ws.onopen = function (event) {
-    console.log("Open websocket")
+    console.log("Open websocket");
 }
 ws.onclose=function(event){
     console.log("Close Websocket");
@@ -28,13 +21,24 @@ ws.onerror = function (event) {
   insertServerMsg("伺服器離線中，請嘗試重新連線。");
 }
 ws.onmessage = function (event) {
-  var msg_json = JSON.parse(event.data);
-  console.log(msg_json);
+  msg = JSON.parse(event.data);
+  var res = (msg.Response).split("sys_");
+  var metadata = msg.Metadata;
+  if( res.length != 1 ){ //sys 1. history 2. token
+    if (res[1] == "history") restoreHistory(metadata);
+    if (res[1] == "token") $.cookie('token', metadata, { expires: 7, path: '/' });
+  }else{ // 1. one-line 2. list
+    if(metadata == "None"){
+      insertServerMsg(res[0],0);
+    }else{
+      insertServerMsg(res[0],metadata);
+    }
+  }
 }
+
 function wsend(msg,type){
   var tmp = {"DataType":type, "Data":msg};
-  console.log(tmp);
-  ws.send(tmp);
+  ws.send(JSON.stringify(tmp));
 }
 
 /*scrollbar*/
@@ -45,15 +49,20 @@ function updateScrollbar() {
   });
 }
 
-function insertServerMsg(msg){
-  $('<li>' + msg + '</li>').appendTo($('#chatBlock'));
+function insertServerMsg(msg,md){
+  if(md == 0){
+    $('<li>' + msg + '</li>').appendTo($('#chatBlock'));
+  }else{
+    var tmp = ""
+    md.forEach(element => tmp += "<button data-opnum="+element+">"+element+"</button><br>");
+    $('<ul id="chatBlock" class="rounded-messages messages-width-large"><li>請選擇你的問題<br>' + tmp + '</ul>').appendTo($('#chatBlock'));
+  }
   updateScrollbar();
   //$('<li class="time-left">'+ getDate() + '</li>').appendTo($('#chatBlock'));
 }
-function insertClientMsg(msg){
+function insertClientMsg(msg,type){
   $('<li class="right-msg">' + msg + '</li>').appendTo($('#chatBlock'));
-  wsend(msg,"raw");
-  //ws.send(msg);
+  wsend(msg,type);
   updateScrollbar();
 }
 
@@ -65,7 +74,7 @@ $(document).ready(function(){
   $('#send').click(function () {
     inputMsg = $('#inputText').val();
     if(inputMsg != ""){
-      insertClientMsg(inputMsg);
+      insertClientMsg(inputMsg,"raw");
       $('#inputText').val(null);
     }
   });
@@ -74,10 +83,28 @@ $(document).ready(function(){
       event.preventDefault();
       inputMsg = $('#inputText').val();
       if(inputMsg != ""){
-        insertClientMsg(inputMsg);
+        insertClientMsg(inputMsg,"raw");
         $('#inputText').val(null);
       }
     }
   });
 });
 
+document.getElementById('chatBlock').addEventListener('click',(e)=>{
+  var clickMsg = (e.target.getAttribute('data-opnum'));
+  if (clickMsg == null) return;
+  insertClientMsg(clickMsg,"clicked");
+})
+
+
+document.getElementById('sys_btn').addEventListener('click',(e)=>{
+  var clickMsg = (e.target.getAttribute('id'));
+  if (clickMsg == null) return;
+  insertClientMsg(clickMsg,"sys");
+})
+
+function restoreHistory(metadata){
+  for (var i = 0 ; i < metadata.length; i++) {
+
+  }
+}
